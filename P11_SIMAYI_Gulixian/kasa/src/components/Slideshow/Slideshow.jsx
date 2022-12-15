3d5cd0652411ca487a2e41bcffea02770b8b68d0
Gvl <gulixiansimayi@gmail.com>
2022-12-15 16:48:38 +0100

    carousel is ok

--- P11_SIMAYI_Gulixian/kasa/src/components/Slideshow/Slideshow.jsx
+++ P11_SIMAYI_Gulixian/kasa/src/components/Slideshow/Slideshow.jsx
@@ -1,33 +1,44 @@
 
-import { useParams } from 'react-router-dom'
+import { useParams} from 'react-router-dom'
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
-
 import data from'../../datas/data.json'
 import './Slideshow.css'
 
 function Slideshow(){
 const {id} = useParams()
+const [currentIndex,setCurrentIndex] = useState(0)
+const carrouselData = data.filter((element) => 
+                            id === element.id ?
+                            element.pictures : null)
+const carrouselPicture = carrouselData.map((pic) => pic.pictures)
+
+const goToPrev = () => {
+  const isFirstPic = currentIndex === 0
+  const newIndex = isFirstPic ? carrouselPicture[0].length-1 : currentIndex-1
+  setCurrentIndex(newIndex)
+}
+
+const goToNext = () => {
+  const isFirstPic = currentIndex === carrouselPicture[0].length-1
+  const newIndex = isFirstPic ? 0 : currentIndex+1
+  setCurrentIndex(newIndex)
+}
 
   return  (
-    <div>
         <div className='carrouselBloc'>
-            <FontAwesomeIcon className='chevronLeft'
-                            icon={faChevronLeft} />
-            <FontAwesomeIcon className='chevronRight'
-                            icon={faChevronRight} />
-            {data.map((element) => 
-            id === element.id ?
-            element.pictures.map((picture,index) =>
-            <div className='SlideshowBloc' key={index} >
-                    <img className='SlideshowImg' src={picture} alt='' />
+            <img className='carrouselImage' src={carrouselPicture[0][currentIndex]} alt="pic" />
+            <div className='carrouselIcon' >
+                <FontAwesomeIcon className='chevronLeft' 
+                                  icon={faChevronLeft} 
+                                  onClick={() => goToPrev()}/>
+                <FontAwesomeIcon className='chevronRight' 
+                                  icon={faChevronRight} 
+                                  onClick={() => goToNext()}/>            
             </div>
-            )
-            :null
-            )}
         </div>
-    </div>
-  )
+   )
 }
 
 export default Slideshow
