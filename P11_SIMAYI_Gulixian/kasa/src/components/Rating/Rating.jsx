d0b85de051b15e202cd8a5aefbc6b52eac389983
Gvl <gulixiansimayi@gmail.com>
2022-12-30 01:12:56 +0100

    correction

--- P11_SIMAYI_Gulixian/kasa/src/components/Rating/Rating.jsx
+++ P11_SIMAYI_Gulixian/kasa/src/components/Rating/Rating.jsx
@@ -1,11 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import './Rating.css'
-// import { useState } from 'react';
 
 function Rating({star}){
-  // let [rating,setRating] = useState(star)
-  console.log(star);
+
   return (
     <div className='allStar' >
       {[...Array(5)].map((stars,index) => {
@@ -21,7 +19,6 @@ function Rating({star}){
             <div key={index}>
               <FontAwesomeIcon 
                 className='star' 
-                // color={'#e4e5e9'} 
                 icon={faStar} />
           </div>
           )
