95bb7c1a51b7c4807c0ff58c30e26cc8fa31e0c1
Gvl <gulixiansimayi@gmail.com>
2023-03-20 16:22:13 +0100

    start from username

---
+++ P12_SIMAYI_Gulixian/sportsee/src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import UserName from "./componants/UserName/UserName";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/user/12")
+      .then((response) => setUser(response.data))
+      .then((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <UserName user={user} />
+    </div>
+  );
+}
+
+export default App;
---
+++ P12_SIMAYI_Gulixian/sportsee/src/componants/UserName/UserName.jsx
@@ -0,0 +1,15 @@
+function UserName({user}) {
+  console.log(user)
+  return (
+    <div>
+      <h1>Bonjour{' '}
+        <span style={{color:'red'}}>
+            {user.data.userInfos.firstName}
+        </span>
+      </h1>
+      <p>Félicitation ! Vous avez explosé vos objectifs hier</p>
+    </div>
+  )
+}
+
+export default UserName
